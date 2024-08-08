b7f7bcda5e53e90395a56df91e514da88e4ba6c0
saif0133 <saif0reda133@gmail.com>
2024-08-08 20:28:41 +0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Adjust this if your app is served from a subdirectory
-})
+  optimizeDeps: {
+    include: [
+      '@mui/material',
+      '@mui/system',
+      '@mui/styled-engine',
+      '@mui/utils',
+    ],
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        '@mui/material/styles',
+        '@mui/system',
+        '@mui/styled-engine',
+        '@mui/utils',
+      ],
+    },
+  },
+});
