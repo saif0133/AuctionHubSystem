7b3b95cb2007b159a1fada72cb117d1b85938166
saif0133 <saif0reda133@gmail.com>
2024-08-08 22:30:07 +0300

    fgf

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import copy from 'vite-plugin-copy'; // Adjust if necessary
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), copy({
-    targets: [
-      { src: 'src/legal.html', dest: 'dist' },
-      { src: 'src/privacy.html', dest: 'dist' },
-      { src: 'src/final project/login-signup page/login.html', dest: 'dist' }
-    ]
-  })],
+  plugins: [react()],
   build: {
     outDir: 'dist', // Ensure this matches your publish directory
     rollupOptions: {
