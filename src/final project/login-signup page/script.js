2974469cb28c97bfecbea8da78b149d295d319b9
saif0133 <saif0reda133@gmail.com>
2024-08-16 16:52:26 +0300

    eslint remove

--- src/final project/login-signup page/script.js
+++ src/final project/login-signup page/script.js
@@ -1,6 +1,7 @@
 // script.js
 let userToken = null;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function toggleForms() {
   const loginForm = document.getElementById("loginForm");
   const signupForm = document.getElementById("signupForm");
