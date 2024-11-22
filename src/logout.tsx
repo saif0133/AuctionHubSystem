d71ec5d1938f58dafc05a487937977a91abe55b0
saif0133 <saif0reda133@gmail.com>
2024-11-22 05:05:46 +0300

    Update logout for the deployment

--- src/logout.tsx
+++ src/logout.tsx
@@ -1,19 +1,42 @@
+// import React, { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// //import removeToken from "./final project/login-signup page/script.js";
+
+// const handleLogout = () => {
+//   localStorage.clear(); // Remove the token from localStorage
+//   window.location.reload();
+// };
+
+// const Logout = () => {
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     handleLogout();
+//     navigate("/");
+//   }, [navigate]);
+
+//   return null;
+// };
+
+// export default Logout;
+
+
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 //import removeToken from "./final project/login-signup page/script.js";
 
 const handleLogout = () => {
   localStorage.clear(); // Remove the token from localStorage
-  window.location.reload();
+ // window.location.reload();
 };
-
 const Logout = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    handleLogout();
-    navigate("/");
-  }, [navigate]);
+  localStorage.clear();
+    //  window.location.reload();
+
+    window.location.href="/"
+
 
   return null;
 };
